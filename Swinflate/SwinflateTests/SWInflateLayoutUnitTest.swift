5035e9b41cbd25eb1111df57faf26d5c6edfbb48
Vlad Iacob <vlad.iacob@p3-group.com>
2018-12-20 09:39:36 +0200

    Add unit tests

---
+++ Swinflate/SwinflateTests/SWInflateLayoutUnitTest.swift
@@ -0,0 +1,32 @@
+//
+//  SWInflateLayoutUnitTest.swift
+//  SwinflateTests
+//
+//  Created by Vlad on 12/20/18.
+//  Copyright © 2018 Vlad Iacob. All rights reserved.
+//
+
+import XCTest
+@testable import Swinflate
+
+final class SWInflateLayoutUnitTest: XCTestCase {
+    
+    private let collectionViewLayout = SWInflateLayout()
+
+    // MARK: - Lifecycle
+    
+    override func setUp() {
+        super.setUp()
+    
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    // MARK: - Functions
+    
+    func testScrollDirection() {
+        XCTAssertEqual(collectionViewLayout.scrollDirection.rawValue, 0)
+    }
+}
---
+++ Swinflate/SwinflateTests/SwinflateTests.m
@@ -0,0 +1,37 @@
+//
+//  SwinflateTests.m
+//  SwinflateTests
+//
+//  Created by Vlad on 12/20/18.
+//  Copyright © 2018 Vlad Iacob. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SwinflateTests : XCTestCase
+
+@end
+
+@implementation SwinflateTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
